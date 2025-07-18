0279e2fdd048e5368617c340b9a31c05dcf26a2a
developerkarnopp8 <developer.karnopp@gmail.com>
2025-07-18 14:56:27 -0300

    Desenvolvendo componente de lista e o base UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { PrimeryButtonComponent } from './_components/primery-button/primery-button.component';
 import { SecundaryButtonComponent } from './_components/secundary-button/secundary-button.component';
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    NavbarComponent, 
+    NavbarComponent,
     PrimeryButtonComponent,
-    SecundaryButtonComponent
-  ],
+    SecundaryButtonComponent,
+    ItemCertificadoComponent,
+    BaseUiComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css' 
 })
