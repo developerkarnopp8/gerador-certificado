75de83a231311d43976db566cc3b475c12a1aca3
developerkarnopp8 <developer.karnopp@gmail.com>
2025-07-18 14:11:55 -0300

    Criando estrutura do botão secundário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { PrimeryButtonComponent } from './_components/primery-button/primery-button.component';
+import { SecundaryButtonComponent } from './_components/secundary-button/secundary-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimeryButtonComponent],
+  imports: [
+    NavbarComponent, 
+    PrimeryButtonComponent,
+    SecundaryButtonComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css' 
 })
