1bf36d01d40f2b0e233df24964e790119540df8d
developerkarnopp8 <developer.karnopp@gmail.com>
2025-07-27 15:35:47 -0300

    Criando interfacer e logica form

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -3,6 +3,7 @@ import { PrimeryButtonComponent } from "../../_components/primery-button/primery
 import { SecundaryButtonComponent } from "../../_components/secundary-button/secundary-button.component";
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
@@ -16,22 +17,25 @@ import { CommonModule } from '@angular/common';
   styleUrl: './certificado-form.component.css'
 })
 export class CertificadoFormComponent {
-  nome: string = '';
   atividade: string = '';
-  data: string = '';
-  atividades: string[] = []
+
+  certificado: Certificado = {
+    nome: '',
+    atividades: [],
+    // data: new Date()
+  };
 
   campoValido(control: NgModel){
     return control.touched && control.invalid;
   }
 
   formValido(){
-    return this.atividades.length > 0 && this.nome.length > 0;
+    return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0;
   }
 
   adicionarAtividade(){
     if (this.atividade.trim() !== '') {
-      this.atividades.push(this.atividade.trim());
+      this.certificado.atividades.push(this.atividade.trim());
       this.atividade = '';
     }
   }
@@ -39,7 +43,14 @@ export class CertificadoFormComponent {
   removeAtividade(index: number) {
     const itemIndex = index;
     if (itemIndex > -1) {
-      this.atividades.splice(itemIndex, 1);
+      this.certificado.atividades.splice(itemIndex, 1);
     }
+  }
+
+  submit(){
+    if (!this.formValido()) {
+      return;
+    }
+    console.log(this.certificado);
   }
 }
